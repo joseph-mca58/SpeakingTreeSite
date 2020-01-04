93e95b5fe600fd36b325f23ef6e915ff9e135152
joseph.mca58 <joseph.mca58@gmail.com>
2020-01-04 17:00:59 +0530

    new changes

--- SpeakingTree/Controllers/HomeController.cs
+++ SpeakingTree/Controllers/HomeController.cs
@@ -3,224 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using SpeakingTree.Models;
-using SpeakingTree.Facade;
-using SpeakingTree.Extensions;
 
 namespace SpeakingTree.Controllers
 {
     public class HomeController : Controller
     {
-        HomeFacade homeFacade = new HomeFacade();
+        // GET: Home
         public ActionResult Index()
         {
             return View();
         }
-
-        #region Login & Register
-
-        [HttpGet]
-        public ActionResult Login()
+        public ActionResult Course()
         {
             return View();
         }
-
-        [HttpPost]
-        public ActionResult Login(User user)
-        {
-            int role = homeFacade.ValidateUser(user.Id, user.Password);
-            if (role > 0)
-            {
-                Session["Role"] = role;
-                Session["UserId"] = user.Id;
-                Session["Username"] = homeFacade.GetUserName(user.Id);
-                switch (role)
-                {
-                    case 1: return RedirectToAction("SuperAdmin", "Admin");
-                    case 2: return RedirectToAction("Index", "Admin");
-                    case 3: return RedirectToAction("Index", "School");
-                    case 4: return RedirectToAction("Index", "Student");
-                }
-            }
-            else
-            {
-                ViewBag.LoginError = "Username OR Password is not valid";
-            }
-            return View();
-        }
-
-        [HttpGet]
-        public ActionResult Register()
-        {
-            User user = new User();
-            var roles = homeFacade.GetRoles();
-            user.lstRole = new SelectList(roles.ToList(), "ID", "Name");
-
-            var states = homeFacade.GetStates();
-            user.lstState = new SelectList(states.ToList(), "ID", "Name");
-
-            var districts = homeFacade.GetDistricts("");
-            user.lstDistrict = new SelectList(districts.ToList(), "ID", "Name");
-
-            user.UserType = "School";
-            user.Id = " ";
-            user.Password = " ";
-            //user.school.ExamOption = "On";
-
-            return View(user);
-        }
-
-        [HttpPost]
-        public ActionResult Register(User user)
-        {
-            //if (ModelState.IsValid)
-            {
-                if (homeFacade.SaveUser(ViewModelExtension.ToBusinessUser(user), out string userid, out string password) > 0)
-                {
-                    //Send email
-                    //Helpers.EmailHelper e = new Helpers.EmailHelper();
-                    //e.SendEmailToUser(user.EmailId, "uid", "", true, "", "");
-                    return RedirectToAction("RegistrationSuccess", new { userId = userid, password = user.Password });
-                }
-            }
-
-            var states = homeFacade.GetStates();
-            user.lstState = new SelectList(states.ToList(), "ID", "Name");
-
-            var districts = homeFacade.GetDistricts("");
-            user.lstDistrict = new SelectList(districts.ToList(), "ID", "Name");
-
-
-
-            return View(user);
-        }
-
-        [HttpGet]
-        public ActionResult RegistrationSuccess(string userId, string password)
-        {
-            ViewBag.RegistrationId = userId;
-            ViewBag.Password = password;
-
-            return View();
-        }
-
-        public JsonResult GetDistrictsByState(string stateId)
-        {
-            List<SelectListItem> states = new List<SelectListItem>();
-            var districts = homeFacade.GetDistricts(stateId);
-            if (districts != null)
-                return Json(new SelectList(districts.ToList(), "ID", "Name"));
-            else
-                return null;
-        }
-
-        [HttpGet]
-        public ActionResult Logout()
-        {
-            Session.Clear();
-            TempData["LogoutMessage"] = "You have successfully logout";
-            return RedirectToAction("Index", "Home");
-        }
-
-        [HttpGet]
-        public ActionResult ForgotPassword(string errMsg = "")
-        {
-            ViewBag.ForgotPasswordError = errMsg;
-            return View();
-        }
-        [HttpPost]
-        public ActionResult ForgotPassword(User user)
-        {
-
-            return RedirectToAction("ShowPassword", new { user.Id, user.ContactNo });
-        }
-
-        [HttpGet]
-        public ActionResult ShowPassword(string id, string ContactNo)
-        {
-            User user = new User();
-            user.Password = homeFacade.GetUserPassword(id, ContactNo);
-            if (string.IsNullOrEmpty(user.Password))
-            {
-
-                return RedirectToAction("ForgotPassword", new { errMsg = "Password could not recovered with the given username and contact number" });
-            }
-            return View(user);
-        }
-
-        [HttpGet]
-        public ActionResult ResetPassword()
+        public ActionResult Event()
         {
             return View();
         }
-        [HttpPost]
-        public ActionResult ResetPassword(User user)
-        {
-            return View();
-        }
-
-        #endregion
-
-        #region Contact us
-
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
-
-        [HttpGet]
-        public ActionResult Enquiry()
-        {
-            return View();
-
-        }
-
-        [HttpPost]
-        public ActionResult Enquiry(Enquiry enquiry)
-        {
-            if (homeFacade.SaveEnquiry(ViewModelExtension.ToBusinessEnquiry(enquiry)) > 0)
-            {
-                //Send email
-            }
-            return View(enquiry);
-        }
-
-        #endregion
-
-        #region Menu items
-
-        [HttpGet]
-        public ActionResult GrammarQuest()
+        public ActionResult Student()
         {
             return View();
         }
-
-        [HttpGet]
-        public ActionResult Gallery()
-        {
-            return View();
-        }
-
-        [HttpGet]
-        public ActionResult Downloads()
-        {
-            return View();
-        }
-        [HttpGet]
-        public ActionResult Careers()
-        {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        #endregion
     }
 }
---
+++ SpeakingTree/Scripts/commonscript.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+    //apply class to selected menu item
+    var currentId;
+    $('.menu').on('click', 'li', function () {
+        currentId = $(this).attr("id");
+        $('.menu li').removeClass('current-menu-item');
+        $("#" + currentId).addClass('current-menu-item');
+    });
+});
